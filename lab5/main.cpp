183b5f11c939118013f11d8eb4f8a63d1e157c02
alex <aleksdorn69@gmail.ru>
2021-06-07 00:43:56 +0700

    balance + execute thread

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -4,7 +4,6 @@
 #include <pthread.h>
 #include <mpi/mpi.h>
 #include <math.h>
-#include <stdbool.h>
 
 #define W 10 // weight parameter
 #define NUM_LISTS 3 // number of lists
@@ -22,7 +21,9 @@ int ProcNum;   // number of processes
 int ProcRank;   // rank of current process
 int RemainingTasks;  // number of remaining tasks
 int ExecutedTasks;   // number of tasks executed
-double GlobalRes = 0;   // global result
+double GlobalRes = 0;   // global result for thread
+double GlobalResSumm = 0; // global global result
+int STOP_RECV_MARKER = -1; // stop key
 
 
 void InitTasks(int *tasksArray, int numTasks, int iterCounter) {//non random weight of task now
@@ -41,33 +42,83 @@ void ExecuteTasks(const int *tasksArray) {
         pthread_mutex_unlock(&mutex);
 
         for (int j = 0; j < currTaskWeight; j++) {
-            globalRes += sin(j);
+            GlobalRes += sin(j);
             usleep(tasksArray[i]);  // sleeping task-weight microseconds make weight of task
         }
 
         ExecutedTasks++;
     }
-    numRemainingTasks = 0;
-}
-
-
-void TasksDistribution() {
-    MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-    //mutex
-    pthread_mutex_init(&mutex, NULL);
-    createThreads();
-    pthread_mutex_destroy(&mutex);
+    RemainingTasks = 0;
 }
 
 
 void* ExecuteRoutine(void* args){
-    //work
+    tasks = (int*)malloc(sizeof(int) * NUM_TASKS);
+    MPI_Status status;
+
+    for (int i = 0; i < NUM_LISTS; i++) {
+
+        InitTasks(tasks, NUM_TASKS, i);
+
+        RemainingTasks = NUM_TASKS;
+        ExecutedTasks = 0;
+        int ExtraTasks = 0;
+
+
+        ExecuteTasks(tasks);//own tasks
+
+        for (int j = 0; j < ProcNum; j++) {//balance
+            if (j != ProcRank) {//not this proc
+                MPI_Send(&ProcRank, 1, MPI_INT, j, 0, MPI_COMM_WORLD);
+
+                // extra tasks
+                MPI_Recv(&ExtraTasks, 1, MPI_INT, j, 1, MPI_COMM_WORLD, &status);
+
+                if (ExtraTasks) {
+
+                    MPI_Recv(tasks, ExtraTasks, MPI_INT, j, 1, MPI_COMM_WORLD, &status);
+
+                    // executing extra tasks
+                    RemainingTasks = ExtraTasks;
+                    ExecuteTasks(tasks);
+                }
+            }
+        }
+
+        MPI_Allreduce(&GlobalRes, &GlobalResSumm, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+        MPI_Barrier(MPI_COMM_WORLD);
+    }
+
+    MPI_Send(&STOP_RECV_MARKER, 1, MPI_INT, ProcRank, 0, MPI_COMM_WORLD);
+    free(tasks);
+    return NULL;
 }
 
 void* ReceiveRoutine(void* args){
-    //work
+    int ShareTasks, requestRank;
+    MPI_Status status;
+
+    while (true) {
+        //rec request. check request. mute. check tasks. share. unmute. done
+        MPI_Recv(&requestRank, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+        if (requestRank == STOP_RECV_MARKER) {
+            pthread_exit(NULL);
+        }
+
+
+        pthread_mutex_lock(&mutex);
+        if (RemainingTasks > NUM_TASKS_TO_SHARE) {
+            ShareTasks = RemainingTasks / 2;
+            RemainingTasks -= ShareTasks;
+
+            MPI_Send(&ShareTasks, 1, MPI_INT, status.MPI_SOURCE, 1, MPI_COMM_WORLD);
+            MPI_Send(&tasks[ShareTasks - 1], ShareTasks, MPI_INT, status.MPI_SOURCE, 1, MPI_COMM_WORLD);
+        } else {
+            ShareTasks = 0;
+            MPI_Send(&ShareTasks, 1, MPI_INT, status.MPI_SOURCE, 1, MPI_COMM_WORLD);
+        }
+        pthread_mutex_unlock(&mutex);
+    }
 }
 
 
@@ -106,6 +157,17 @@ int CreateThreads() {
         return 1;
     }
     return 0;
+}
+
+
+void TasksDistribution() {
+    MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
+    MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
+
+    //mutex
+    pthread_mutex_init(&mutex, NULL);
+    CreateThreads();
+    pthread_mutex_destroy(&mutex);
 }
 
 
